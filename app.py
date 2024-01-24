45595e8a1a51fc69bc32a16a51467aa3ec539165
hllee <hllee0730@gmail.com>
2024-01-24 16:38:50 +0800

    Add database support with Flask-SQLAlchemy.

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import os
 import sys
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
+import click
 
 WIN = sys.platform.startswith('win')
 
@@ -9,23 +10,6 @@ if WIN:
     prefix = 'sqlite:///'
 else:
     prefix = 'sqlite:////'
-
-
-
-# define dataframe
-name = 'hllee'
-movies = [
-    {'title': 'My Neighbor Totoro', 'year': '1988'},
-    {'title': 'Dead Poets Society', 'year': '1989'},
-    {'title': 'A Perfect World', 'year': '1993'},
-    {'title': 'Leon', 'year': '1994'},
-    {'title': 'Mahjong', 'year': '1996'},
-    {'title': 'Swallowtail Butterfly', 'year': '1996'},
-    {'title': 'King of Comedy', 'year': '1999'},
-    {'title': 'Devils on the Doorstep', 'year': '1999'},
-    {'title': 'WALL-E', 'year': '2008'},
-    {'title': 'The Pork of Music', 'year': '2012'},
-]
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = prefix + os.path.join(app.root_path, 'data.db')
@@ -44,7 +28,46 @@ class Movie(db.Model):  # 表名: movie
     title = db.Column(db.String(60))  # 电影标题
     year = db.Column(db.String(4))  # 电影年份
 
+@app.cli.command()
+def forge():
+    """Generate dummy data."""
+    db.create_all()
+
+    # 全局的两个变量移动到这个函数内
+    name = 'hllee'
+    movies = [
+        {'title': 'My Neighbor Totoro', 'year': '1988'},
+        {'title': 'Dead Poets Society', 'year': '1989'},
+        {'title': 'A Perfect World', 'year': '1993'},
+        {'title': 'Leon', 'year': '1994'},
+        {'title': 'Mahjong', 'year': '1996'},
+        {'title': 'Swallowtail Butterfly', 'year': '1996'},
+        {'title': 'King of Comedy', 'year': '1999'},
+        {'title': 'Devils on the Doorstep', 'year': '1999'},
+        {'title': 'WALL-E', 'year': '2008'},
+        {'title': 'The Pork of Music', 'year': '2012'},
+    ]
+
+    user = User(name=name)
+    db.session.add(user)
+    for m in movies:
+        movie = Movie(title=m['title'], year=m['year'])
+        db.session.add(movie)
+
+    db.session.commit()
+    click.echo('Done.')
+
+@app.cli.command()
+@click.option('--drop', is_flag=True, help='Create after drop.')
+def initdb(drop):
+    if drop:
+        db.drop_all()
+        click.echo('Droping former database.')
+    db.create_all()
+    click.echo('Initialising database.')
 
 @app.route('/')
 def index():
-    return render_template('index.html', name=name, movies=movies)
+    user = User.query.first()
+    movies = Movie.query.all()
+    return render_template('index.html', user=user, movies=movies)
