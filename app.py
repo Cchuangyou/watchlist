cc94da210d2e7f4ab6a823709d1cd78816eac499
hllee <hllee0730@gmail.com>
2024-01-24 15:52:03 +0800

    commit half

--- app.py
+++ app.py
@@ -1,6 +1,16 @@
+import os
+import sys
 from flask import Flask, render_template
-from markupsafe import escape
-from flask import url_for
+from flask_sqlalchemy import SQLAlchemy
+
+WIN = sys.platform.startswith('win')
+
+if WIN:
+    prefix = 'sqlite:///'
+else:
+    prefix = 'sqlite:////'
+
+
 
 # define dataframe
 name = 'hllee'
@@ -18,6 +28,22 @@ movies = [
 ]
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = prefix + os.path.join(app.root_path, 'data.db')
+app.config['SQLALCHEMY_TRACK_MODIFICATION'] = False # 关闭对模型修改的监控
+# loading config
+db = SQLAlchemy(app)
+
+# create database model
+class User(db.Model):  # 表名: user
+    id = db.Column(db.Integer, primary_key=True)  # 主键
+    name = db.Column(db.String(20))  # 用户名
+
+
+class Movie(db.Model):  # 表名: movie
+    id = db.Column(db.Integer, primary_key=True)  # 主键
+    title = db.Column(db.String(60))  # 电影标题
+    year = db.Column(db.String(4))  # 电影年份
+
 
 @app.route('/')
 def index():
